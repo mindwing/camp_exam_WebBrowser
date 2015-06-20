5ac6925c94e5687db995e029e85122894833af7d
mindwing <mindwing@gmail.com>
2015-06-20 14:16:59 +0900

    recover codes

--- camp_exam_WebBrowser/src/com/example/camp_exam_webbrowser/MainActivity.java
+++ camp_exam_WebBrowser/src/com/example/camp_exam_webbrowser/MainActivity.java
@@ -1,35 +1,66 @@
 package com.example.camp_exam_webbrowser;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private EditText textUrl;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		textUrl = (EditText) findViewById(R.id.text_url);
+		textUrl.setOnKeyListener(new OnKeyListener() {
+
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if (event.getAction() != KeyEvent.ACTION_DOWN) {
+					return false;
+				}
+
+				boolean processed = false;
+
+				switch (keyCode) {
+				case KeyEvent.KEYCODE_ENTER:
+					Toast.makeText(
+							MainActivity.this,
+							textUrl.getText().toString() + ": "
+									+ event.getAction(), Toast.LENGTH_SHORT)
+							.show();
+
+					processed = true;
+
+					break;
+
+				default:
+
+					break;
+				}
+
+				return processed;
+			}
+
+		});
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
